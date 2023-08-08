b84474157c4a212f853a15c5995ed8272821451f
Victoria Martinez <vicotita1999@gmail.com>
2023-08-08 18:47:13 -0300

    Alert div

---
+++ eventos.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const div = document.querySelector("div");
+
+    div.addEventListener("click", () => {
+
+        alert ('Hola! Soy el div');
+    
+    });
+
+});
+
+
+
